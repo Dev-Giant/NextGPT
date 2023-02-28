5df44386501a3bc6637e63634967a40a5d31cb8f
Dev-Giant <Artyom080396@outlook.com>
2023-02-28 13:08:07 +0100

    fix for types

--- types/RSSTypes.ts
+++ types/RSSTypes.ts
@@ -1,35 +1,35 @@
 // These are the types generated from our blog's RSS feed at https://www.itmagination.com/blog/rss.xml
 
 export interface RSS {
-    channel:        Channel;
-    _version:       string;
-    "_xmlns:atom":  string;
+    channel: Channel;
+    _version: string;
+    "_xmlns:atom": string;
     "_xmlns:media": string;
 }
 
 export interface Channel {
-    title:       string;
-    link:        Array<LinkClass | string>;
+    title: string;
+    link: Array<LinkClass | string>;
     description: string;
-    pubDate:     string;
-    ttl:         string;
-    generator:   string;
-    item:        Item[];
+    pubDate: string;
+    ttl: string;
+    generator: string;
+    item: Item[];
 }
 
 export interface Item {
-    title:       string;
-    link:        string;
-    guid:        string;
+    id: number;
+    title: string;
+    link: string;
     description: string;
-    pubDate:     string;
-    content:     Content;
-    thumbnail:   Thumbnail;
+    publishedAt: string;
+    imageUrl: string;
+    thumbnailUrl: string;
 }
 
 export interface Content {
-    _url:     string;
-    _medium:  Medium;
+    _url: string;
+    _medium: Medium;
     __prefix: Prefix;
 }
 
@@ -42,30 +42,23 @@ export enum Medium {
 }
 
 export interface Thumbnail {
-    _url:     string;
+    _url: string;
     __prefix: Prefix;
 }
 
 export interface LinkClass {
-    _href:    string;
-    _rel:     string;
-    _type:    string;
+    _href: string;
+    _rel: string;
+    _type: string;
     __prefix: string;
 }
 
-export const initialItemState: Item  = {
+export const initialItemState: Item = {
+    id: 1,
     title: "",
     link: "",
-    guid: "",
     description: "",
-    pubDate: "",
-    content: {
-      _url: "",
-      _medium: Medium.Image,
-      __prefix: Prefix.Media,
-    },
-    thumbnail: {
-      _url: "",
-      __prefix: Prefix.Media,
-    },
+    publishedAt: "",
+    imageUrl: "",
+    thumbnailUrl: ""
 }
